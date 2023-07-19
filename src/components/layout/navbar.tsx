bd3edb9bd4ab2908ceb065eacb06b8c2a93e5451
Eric Lin <40213852eric@gmail.com>
2023-07-19 14:00:38 +0800

    fix:countdown error
    feat: register did

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -7,15 +7,15 @@ export default function Navbar() {
   return (
     <div className="navbar bg-#fff5d7 shadow-md p-0">
       <div className="navbar-start items-center flex">
-        <img src="bee.png" className="object-cover w-24 ml-10" alt="logo"/>
-        <a className="btn btn-ghost normal-case text-2xl p-0 text-black" onClick={()=>router.push('/')}>BeePoll</a>
+        <img src="bee.png" className="object-cover w-24 ml-10" alt="logo" />
+        <a className="btn btn-ghost normal-case text-2xl p-0 text-black" onClick={() => router.push('/')}>BeePoll</a>
       </div>
       <div className="navbar-center hidden lg:flex">
       </div>
       <div className="navbar-end mr-5">
         <div className="flex items-center">
-          <Darkmode/>
-          <ConnectKitButton/>
+          <Darkmode />
+          <ConnectKitButton />
           {/* <Login/> */}
         </div>
       </div>
