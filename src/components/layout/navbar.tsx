8cee35c74aad9f38adf769a5a326f587ec8c9ea4
musicat1020 <musicat1020@gmail.com>
2023-07-20 14:53:38 +0800

    fix: add register hook

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,30 @@
 import { ConnectKitButton } from "connectkit"
 import { useRouter } from "next/router"
 import Darkmode from "../darkmode"
+import { useAccount } from "wagmi"
+import { useEffect } from "react"
+import { createDid, registerDID } from "../../services/did"
 
+import { signMessage } from "@wagmi/core";
+import React from "react"
 export default function Navbar() {
   const router = useRouter()
+  const { address, isConnected } = useAccount()
+
+  React.useEffect(() => {
+    if (isConnected && address) {
+      createDid(address).then((didDocument) => {
+        const message = JSON.stringify(didDocument)
+        signMessage({ message }).then((signature) => {
+          registerDID(didDocument, signature).then((res) => {
+            console.log(res)
+          })
+        })
+      })
+    }
+  }, [address, isConnected])
+
+
   return (
     <div className="navbar bg-#fff5d7 shadow-md p-0">
       <div className="navbar-start items-center flex">
@@ -16,7 +37,6 @@ export default function Navbar() {
         <div className="flex items-center">
           <Darkmode />
           <ConnectKitButton />
-          {/* <Login/> */}
         </div>
       </div>
     </div>
