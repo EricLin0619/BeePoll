a5b29010a5621ff675ea38f91103b6b194899107
musicat1020 <musicat1020@gmail.com>
2023-07-21 13:55:55 +0800

    fix: use NEXT_PUBLIC_API_SECRET_KEY directly

--- src/services/did.ts
+++ src/services/did.ts
@@ -4,7 +4,7 @@ export const getAccessToken = async () => {
     const url = 'https://api.entity.hypersign.id/api/v1/app/oauth';
     const headers = {
         'accept': 'application/json',
-        'X-Api-Secret-Key': process.env.NEXT_PUBLIC_API_SECRET_KEY,
+        'X-Api-Secret-Key': '1fae14d6e06b702c5b825e469fc2b.7e218298064c0cc5c00aedcfe73cb2c501b60cea1209245a471e4c53334c6dca457635a7884658350fe602aa21de03fbb',
     };
 
     try {
