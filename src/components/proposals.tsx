3ad2d13b2a21116b37ea4e511a091a87ca3db848
Eric Lin <40213852eric@gmail.com>
2023-07-25 14:55:58 +0800

    feat:let didcard can display correct address and did

--- src/components/proposals.tsx
+++ src/components/proposals.tsx
@@ -4,7 +4,12 @@ import SelectBar from "./selectBar"
 export default function Proposals() {
   return (
     <div className="mt-10">
-      <p className="font-mono text-black font-bold text-3xl ml-5 mt-3 dark:text-white">VOTING</p>
+      <div className="flex mt-10 w-1/2">
+        <p className="font-mono text-black font-bold text-3xl ml-5 mt-3 dark:text-white">VOTING</p>
+        <button className="btn btn-outline h-1/3 p-1 btn-warning ml-auto ">
+          New Vote
+        </button>
+      </div>
       <div className="divider ml-5 mr-auto w-1/2 mt-0 mb-4"></div> 
       <SelectBar/>
       <div className="mb-10 grid xl-[1320px] xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center">
@@ -13,6 +18,7 @@ export default function Proposals() {
         <ProposalCard/>
         <ProposalCard/>
       </div>
+      
     </div>
   )
 }
