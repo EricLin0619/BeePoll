fc224ee3395c69915d5408052a0ab88ed4d312c9
kidneyweak <kidneyweak@gmail.com>
2023-08-07 17:38:24 +0800

    feat: deploy contract

--- packages/sol-bee-poll/scripts/tasks/deployProcess.ts
+++ packages/sol-bee-poll/scripts/tasks/deployProcess.ts
@@ -22,3 +22,36 @@ task('deploy:contract', 'Deploy contract')
     await deployContract.deployed()
   },
   )
+
+task('deploy:beepoll', 'Deploy contract')
+  .addFlag('verify', 'Validate contract after deploy')
+  .setAction(async ({verify}, hre) => {
+    await hre.run('compile')
+    const [signer] = await hre.ethers.getSigners()
+    const contractFactory = await hre.ethers.getContractFactory('contracts/BeePoll.sol:BeePoll')
+    // if you mint in constructor, you need to add value in deploy function
+    const deployContract = await contractFactory.connect(signer).deploy()
+    console.log(`BeePoll.sol deployed to ${deployContract.address}`)
+
+    const address = {
+      main: deployContract.address,
+    }
+    const addressData = JSON.stringify(address)
+    writeFileSync(`scripts/address/${hre.network.name}/`, 'BeePoll.json', addressData)
+
+    const deployTX = await deployContract.deployed()
+    deployTX.wait(3)
+    if (verify) {
+      console.log('verifying contract...')
+      try {
+        await hre.run('verify:verify', {
+          address: deployContract.address,
+          constructorArguments: [],
+          contract: 'contracts/BeePoll.sol:BeePoll',
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+  )
