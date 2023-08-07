aff0e918344450f49f15df299223f26677e983b6
musicat1020 <musicat1020@gmail.com>
2023-08-07 11:04:55 +0800

    feat: getAuth0Urls

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,55 +1,39 @@
-import { handleAuth, handleCallback, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { type NextApiHandler } from "next";
+import { getAuth0Instance, getAuth0Urls } from "../../../server/auth";
 
+const handler: NextApiHandler = (req, res) => {
+    const instance = getAuth0Instance(req);
 
-const audience = process.env.AUTH0_AUDIENCE;
-const scope = process.env.AUTH0_SCOPE;
+    const instanceHandler = instance.handleAuth({
+        login: async (req: any, res: any) => {
+            const { redirectUri, returnTo } = getAuth0Urls(req);
 
-function getUrls(req: NextApiRequest) {
-    const host = req.headers['host'];
-    const protocol = process.env.VERCEL_URL ? 'https' : 'http';
-    const redirectUri = `${protocol}://${host}/api/auth/callback`;
-    const returnTo = `${protocol}://${host}`;
-    return {
-        redirectUri,
-        returnTo
-    };
-}
-
-export default handleAuth({
-    async callback(req: NextApiRequest, res: NextApiResponse) {
-        try {
-            console.log("hihi")
-            const { redirectUri } = getUrls(req);
-            console.log("callback uri", redirectUri)
-            await handleCallback(req, res, { redirectUri: redirectUri });
-        } catch (error: any) {
-            res.status(error.status || 500).end(error.message);
-        }
-    },
-
-    async login(req: NextApiRequest, res: NextApiResponse) {
-        try {
-            const { redirectUri, returnTo } = getUrls(req);
-
-            await handleLogin(req, res, {
+            await instance.handleLogin(req, res, {
                 authorizationParams: {
-                    screen_hint: "login",
-                    // audience: audience,
-                    // scope: scope,
-                    redirect_uri: redirectUri
+                    redirect_uri: redirectUri,
                 },
-                returnTo: returnTo
+                returnTo: returnTo,
             });
-        } catch (error: any) {
-            res.status(error.status || 400).end(error.message);
-        }
-    },
+        },
+        callback: async (req: any, res: any) => {
+            const { redirectUri, returnTo } = getAuth0Urls(req);
 
-    async logout(req: NextApiRequest, res: NextApiResponse) {
-        const { returnTo } = getUrls(req);
-        await handleLogout(req, res, {
-            returnTo: returnTo
-        });
-    }
-});
+            await instance.handleCallback(req, res, {
+                authorizationParams: {
+                    redirect_uri: redirectUri,
+                }
+            });
+        },
+        logout: async (req: any, res: any) => {
+            const { redirectUri, returnTo } = getAuth0Urls(req);
+
+            await instance.handleLogout(req, res, {
+                returnTo: returnTo,
+            });
+        },
+    });
+
+    instanceHandler(req, res);
+};
+
+export default handler;
