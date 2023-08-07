fcc5701d30ebaf4fed07ee8734d7084e186d1bac
musicat1020 <musicat1020@gmail.com>
2023-08-07 13:39:15 +0800

    fix: update auth0 version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ import { ThemeProvider } from "next-themes";
 
 function App({ Component, pageProps: session, ...pageProps }: AppProps) {
   return (
-    <UserProvider loginUrl="/api/auth/login">
+    <UserProvider>
       <ThemeProvider attribute="class">
         <WagmiConfig client={client}>
           <ConnectKitProvider>
