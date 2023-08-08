248d2c02fdca3c3db0d17f829d26ef6b543bc217
Ubuntu <ubuntu@ip-10-0-2-10.us-west-2.compute.internal>
2023-08-08 08:06:31 +0000

    fix: voteButton modal bug and vc input area

--- src/components/button/verifyVcButton.tsx
+++ src/components/button/verifyVcButton.tsx
@@ -1,14 +1,6 @@
 export default function VerifyVcButton() {
     return (
       <div>
-        {/* Open the modal using ID.showModal() method */}
-        {/* onClick={() => {
-            if (document) {
-              (
-                document.getElementById("my_modal_2") as HTMLFormElement
-              ).showModal();
-            }
-          }} */}
         <button className="btn btn-outline px-2 btn-info" onClick={() => {
             if (document) {
               (
@@ -17,38 +9,14 @@ export default function VerifyVcButton() {
             }
           }}>Verify VC</button>
         <dialog id="my_modal_2" className="modal">
-          <form method="dialog" className="modal-box p-8">
+          <form method="dialog" className="modal-box p-8 h-1/2 flex flex-col">
             <h3 className="font-bold text-lg text-white">
               Please input your VC data
             </h3>
-            <div className="mt-2">
-              <input
-                type="text"
-                placeholder="VC ID"
-                className="input input-bordered w-full my-4"
-              />
-              <input
-                type="text"
-                placeholder="name"
-                className="input input-bordered w-full my-4"
-              />
-              <input
-                type="text"
-                placeholder="nickname"
-                className="input input-bordered w-full my-4"
-              />
-              <input
-                type="text"
-                placeholder="picture"
-                className="input input-bordered w-full my-4"
-              />
-              <input
-                type="text"
-                placeholder="sub"
-                className="input input-bordered w-full my-4"
-              />
+            <div className="mt-5 mb-6 h-full">
+              <textarea className="textarea textarea-bordered h-full w-full" placeholder="Your VC"></textarea>
             </div>
-            <button className="btn btn-outline btn-success my-2 btn-info px-2">Verify</button>
+            <button className="btn btn-outline btn-success btn-info px-2">Verify</button>
           </form>
           <form method="dialog" className="modal-backdrop">
             <button>close</button>
