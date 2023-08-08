cb51c1196314b5f70fed5c69389a327925d2855e
kidneyweak <kidneyweak@gmail.com>
2023-08-08 11:26:07 +0800

    feat: zk circuits

--- packages/sol-bee-poll/scripts/tasks/deployProcess.ts
+++ packages/sol-bee-poll/scripts/tasks/deployProcess.ts
@@ -40,14 +40,50 @@ task('deploy:beepoll', 'Deploy contract')
     writeFileSync(`scripts/address/${hre.network.name}/`, 'BeePoll.json', addressData)
 
     const deployTX = await deployContract.deployed()
-    deployTX.wait(3)
+    
     if (verify) {
+      deployTX.wait(3)
       console.log('verifying contract...')
       try {
         await hre.run('verify:verify', {
           address: deployContract.address,
           constructorArguments: [],
           contract: 'contracts/BeePoll.sol:BeePoll',
+        })
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  },
+  )
+
+  task('deploy:himitsu', 'Deploy contract himitsu')
+  .addFlag('verify', 'Validate contract after deploy')
+  .setAction(async ({verify}, hre) => {
+    await hre.run('compile')
+    const [signer] = await hre.ethers.getSigners()
+    const contractFactory = await hre.ethers.getContractFactory('contracts/HimitsuVerifier.sol:Verifier')
+    // if you mint in constructor, you need to add value in deploy function
+    const deployContract = await contractFactory.connect(signer).deploy()
+    console.log(`HimitsuVerifier.sol deployed to ${deployContract.address}`)
+
+    const address = {
+      main: deployContract.address,
+    }
+    const addressData = JSON.stringify(address)
+    writeFileSync(`scripts/address/${hre.network.name}/`, 'HimitsuVerifier.json', addressData)
+
+    const deployTX = await deployContract.deployed()
+    
+    if (verify) {
+      console.log('verifying contract...')
+      await deployContract.deployTransaction.wait(5)
+
+      try {
+        await hre.run('verify:verify', {
+          address: deployContract.address,
+          constructorArguments: [],
+          contract: 'contracts/HimitsuVerifier.sol:Verifier',
         })
       } catch (e) {
         console.log(e)
