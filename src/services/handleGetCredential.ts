7f5e83c610af1f22e201a059565cd90594b98e2a
musicat1020 <musicat1020@gmail.com>
2023-08-09 15:00:23 +0800

    feat: return vc credential hash

--- src/services/handleGetCredential.ts
+++ src/services/handleGetCredential.ts
@@ -9,10 +9,14 @@ export async function handleGetCredential(did: string, address: `0x${string}`, g
         console.log("create vc")
         const webAuthnId = await onCreate(did);
         console.log("webAuthnId", webAuthnId)
-        const vcId = await issueVc(did, githubUser.user, webAuthnId);
+        const vc = await issueVc(did, githubUser.user, webAuthnId);
+        console.log("vc", vc)
+        const vcId = vc.credentialStatus.claim.id
         const userSub = githubUser.user?.sub as string;
         console.log("github user sub", userSub)
         const data = await addVcToDid(address, vcId, userSub)
         console.log(data)
+        return { vc, webAuthnId }
     }
+    return { vc: "", webAuthnId: "" }
 }
