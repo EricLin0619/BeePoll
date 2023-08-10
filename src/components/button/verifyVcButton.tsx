3ab5a83e33f996adab246f7bf53c68da9c9a31a1
musicat1020 <musicat1020@gmail.com>
2023-08-10 14:35:29 +0800

    fix: change verify vc work flow

--- src/components/button/verifyVcButton.tsx
+++ src/components/button/verifyVcButton.tsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { onGet } from "../../services/did/webAuthnUtils";
+import { toast } from "react-toastify";
 
 export default function VerifyVcButton(props: any) {
   const [vc, setVc] = useState("");
   const handleButtonClick = async () => {
-    const localWebAuthnId = await onGet()
-    console.log("localWebAuthnId", localWebAuthnId)
-    if (document && localWebAuthnId) {
+    if (document) {
       (
         document.getElementById("my_modal_2") as HTMLFormElement
       ).showModal();
@@ -15,8 +14,21 @@ export default function VerifyVcButton(props: any) {
 
   async function handleVerify() {
     const data = JSON.parse(vc);
-    const inputs = data.credentialStatus.credentialHash
-    props.setCredentialHash(inputs);
+    const localWebAuthnId = await onGet()
+    const vcWebAuthnId = data.credentialDocument.credentialSubject.webAuthnId
+    if (localWebAuthnId === vcWebAuthnId) {
+      const inputs = data.credentialStatus.credentialHash
+      props.setCredentialHash(inputs);
+    }
+    else {
+      toast("Incorrect WebAuthnId", {
+        hideProgressBar: true,
+        theme: "dark",
+        autoClose: 2000,
+        toastId: "fail1",
+
+      })
+    }
   }
 
   return (
@@ -30,7 +42,7 @@ export default function VerifyVcButton(props: any) {
             Please input your VC data
           </h3>
           <div className="mt-5 mb-6 h-full">
-            <textarea className="textarea textarea-bordered h-full w-full" placeholder="Your VC" onChange={(e)=>{setVc(e.target.value)}}></textarea>
+            <textarea className="textarea textarea-bordered h-full w-full" placeholder="Your VC" onChange={(e) => { setVc(e.target.value) }}></textarea>
           </div>
           <button className="btn btn-outline btn-success btn-info px-2" onClick={handleVerify}>Verify</button>
         </form>
